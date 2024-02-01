2638bf3fbe3078e4b62958fdbc15e3f625d9c168
You-loop-great <silvia.cg12@gmail.com>
2024-02-01 16:26:42 +0100

    Finalizo las clases del paquete vista (falta solucionar un par de errores) y el Mainapp (a solucionar errores)

--- src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
@@ -1,11 +1,97 @@
 package org.iesalandalus.programacion.reservashotel;
 
 
+import org.iesalandalus.programacion.reservashotel.dominio.Habitacion;
+import org.iesalandalus.programacion.reservashotel.dominio.Huesped;
+import org.iesalandalus.programacion.reservashotel.dominio.TipoHabitacion;
+import org.iesalandalus.programacion.reservashotel.negocio.Huespedes;
+import org.iesalandalus.programacion.reservashotel.negocio.Habitaciones;
+import org.iesalandalus.programacion.reservashotel.negocio.Reservas;
+import org.iesalandalus.programacion.reservashotel.vista.Opcion;
+import org.iesalandalus.programacion.reservashotel.vista.Consola;
+
+import java.time.LocalDate;
+
 public class MainApp {
 
     public static final int CAPACIDAD = 0;
+    private Reservas reservas;
+    private Huespedes huespedes;
+    private Habitaciones habitaciones;
+
     
     public static void main(String[] args) {
+
         System.out.println("Hasta luego!!!!");
+
     }
+
+    private static void ejecutarOpcion(Opcion opcion){
+
+    }
+
+    private static void insertarHuesped(){
+
+    }
+
+    private static void buscarHuesped(){
+
+    }
+
+    private static void borrarHuesped(){
+
+    }
+
+    private static void mostrarHuesoedes(){
+
+    }
+
+    private static void insertarHabitacion(){
+
+    }
+
+    private static void buscarHabitacion(){
+
+    }
+
+    private static void borrarHabitacion(){
+
+    }
+
+    private static void mostrarHabitaciones(){
+
+    }
+
+    private static void insertarReserva(){
+
+    }
+
+    private static void listarReservas(Huesped huesped){
+
+    }
+
+    private static void listarReservas(TipoHabitacion tipoHabitacion){
+
+    }
+
+    private static getReservasAnulables(coleccionReservas[] reservasAnular){
+
+        return coleccionReservas[];
+    }
+
+    private static void anularReserva(){
+
+    }
+
+    private static void mostrarReservas(){
+
+    }
+
+    private static consultarDisponibilidad(TipoHabitacion tipoHabitacion, LocalDate fechaInicioReserva, LocalDate fechaFinReserva){
+
+        return Habitacion;
+    }
+
+
+
 }

43f2d6dbecb9a92d6974049750a1018c449bf2ed
You-loop-great <silvia.cg12@gmail.com>
2024-02-09 18:29:01 +0100

    Finalizo clase Mainapp

--- src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
@@ -1,31 +1,32 @@
 package org.iesalandalus.programacion.reservashotel;
 
 
-import org.iesalandalus.programacion.reservashotel.modelo.dominio.Habitacion;
-import org.iesalandalus.programacion.reservashotel.modelo.dominio.Huesped;
-import org.iesalandalus.programacion.reservashotel.modelo.dominio.Reserva;
-import org.iesalandalus.programacion.reservashotel.modelo.dominio.TipoHabitacion;
-import org.iesalandalus.programacion.reservashotel.modelo.negocio.Huespedes;
-import org.iesalandalus.programacion.reservashotel.modelo.negocio.Habitaciones;
-import org.iesalandalus.programacion.reservashotel.modelo.negocio.Reservas;
-import org.iesalandalus.programacion.reservashotel.vista.Opcion;
-import org.iesalandalus.programacion.reservashotel.vista.Consola;
-import org.iesalandalus.programacion.utilidades.Entrada;
-
-import javax.naming.OperationNotSupportedException;
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Comparator;
+import org.iesalandalus.programacion.reservashotel.controlador.Controlador;
+import org.iesalandalus.programacion.reservashotel.modelo.negocio.*;
+import org.iesalandalus.programacion.reservashotel.vista.Vista;
 
 public class MainApp {
-
-
-
 
     public static void main(String[] args) {
 
 
+        // Crea instancias de modelo, vista y controlador
+
+        Reservas reservas = new Reservas(10);
+        Huespedes huespedes = new Huespedes(10);
+        Habitaciones habitaciones = new Habitaciones(10);
+        Controlador controlador = new Controlador(null, null);
+        Vista vista = new Vista();
+
+        // Configura la vista con el controlador
+
+        vista.setControlador(controlador);
+
+        // Inicia la aplicación invocando el método comenzar del controlador
+
+        controlador.comenzar();
+    }
+
 
     }
 
-}

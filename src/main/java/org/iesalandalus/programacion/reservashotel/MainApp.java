693e7365893ce58542e4a32808fc75ebd0622029
You-loop-great <silvia.cg12@gmail.com>
2024-02-09 18:38:14 +0100

    Cambios en el Mainapp

--- src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
@@ -2,31 +2,25 @@ package org.iesalandalus.programacion.reservashotel;
 
 
 import org.iesalandalus.programacion.reservashotel.controlador.Controlador;
-import org.iesalandalus.programacion.reservashotel.modelo.negocio.*;
+import org.iesalandalus.programacion.reservashotel.modelo.Modelo;
 import org.iesalandalus.programacion.reservashotel.vista.Vista;
 
 public class MainApp {
 
     public static void main(String[] args) {
 
-
         // Crea instancias de modelo, vista y controlador
 
-        Reservas reservas = new Reservas(10);
-        Huespedes huespedes = new Huespedes(10);
-        Habitaciones habitaciones = new Habitaciones(10);
-        Controlador controlador = new Controlador(null, null);
+        Modelo modelo = new Modelo();
         Vista vista = new Vista();
+        Controlador controlador = new Controlador(modelo, vista);
 
         // Configura la vista con el controlador
-
         vista.setControlador(controlador);
 
         // Inicia la aplicación invocando el método comenzar del controlador
-
         controlador.comenzar();
     }
 
-
-    }
+}
 
